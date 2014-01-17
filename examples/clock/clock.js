d3054581d057b8d37618efd4ec0c24ed7246243e
Andy Ramakrishna <andyr.dev@gmail.com>
2014-01-16 17:56:11 -0800

    added initial times, changed scope of rotate()

--- examples/clock/clock.js
+++ examples/clock/clock.js
@@ -1,20 +1,29 @@
 
+// Compute initial state from current time
+var now = new Date();
 
-var sa=0, ma=0, ha=0,
-    s_el = $('.second-hand'),
+var now_s = now.getSeconds();
+var now_m = now.getMinutes();
+var now_h = now.getHours() % 12;
+
+var sa = now_s * 6;
+var ma = (now_s * 6/60) + now_m * 6;
+var ha = (now_s * 30/3600) + (now_m * 30/60) + (now.getHours() % 12)*30;
+
+var s_el = $('.second-hand'),
     m_el = $('.minute-hand'),
     h_el = $('.hour-hand');
 var interval = 25;
 
+function rotate(el, angle) {
+  el.css({ transform: 'rotate('+angle+'deg)' });
+};
+  
 setInterval(function () { 
   sa += 6/(1000/interval) % 360; // 6deg/s
   ma += 6/(60 * 1000/interval) % 360; // 6deg/min
   ha += 30/(3600 * 1000/interval) % 360; // 30deg/1hr
   
-  function rotate(el, angle) {
-    el.css({ transform: 'rotate('+angle+'deg)' });
-  };
-
   rotate(s_el, sa);
   rotate(m_el, ma);
   rotate(h_el, ha);
